279e3b82680059a3dbb29b5dca49a088dc5ab62e
lami <dorella.mphan@gmail.com>
2025-10-13 15:36:42 +0200

    testing Rest

--- src/main/java/com/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/taskmanager/TaskManagerApplication.java
@@ -1,11 +1,19 @@
-// TaskManagerApplication.java
 package com.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TaskManagerApplication {
+
+  @GetMapping("/helloworld")
+  public String sayHello() {
+    return "Hello World!";
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(TaskManagerApplication.class, args);
   }
