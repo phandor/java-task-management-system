7357138958cf21d8f494d39fcdd14c15d41e6fb4
lami <dorella.mphan@gmail.com>
2025-10-16 17:23:48 +0200

    add user entity constructor, getter, setter

--- src/main/java/com/taskmanager/entity/User.java
+++ src/main/java/com/taskmanager/entity/User.java
@@ -41,4 +41,90 @@ public class User {
   @OneToMany(mappedBy = "assignedUser", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<Task> tasks;
 
+  @PrePersist
+  protected void onCreate() {
+    this.createdAt = LocalDate.now();
+    this.updatedAt = LocalDate.now();
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    this.updatedAt = LocalDate.now();
+  }
+
+  // constructor
+  public User(
+      String username,
+      String email,
+      String firstName,
+      String lastName,
+      Role role) {
+    this.username = username;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.role = role;
+  }
+
+  // getters and setters
+  public Long getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  public LocalDate getCreatedAt() {
+    return createdAt;
+  }
+
+  public LocalDate getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public List<Task> getTasks() {
+    return tasks;
+  }
+
+  public void setTasks(List<Task> tasks) {
+    this.tasks = tasks;
+  }
+
 }
