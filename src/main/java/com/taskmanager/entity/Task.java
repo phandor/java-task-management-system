2c29683309517e12c16c47b0635ff3e043470ade
lami <dorella.mphan@gmail.com>
2025-10-16 17:17:54 +0200

    add task entity constructor, getter, setter

--- src/main/java/com/taskmanager/entity/Task.java
+++ src/main/java/com/taskmanager/entity/Task.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
@@ -24,6 +26,10 @@ public class Task {
 
   private String description;
 
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User assignedUser;
+
   @Enumerated(EnumType.STRING)
   private Status status = Status.PENDING;
 
@@ -38,5 +44,87 @@ public class Task {
 
   @Column(name = "due_date")
   private String dueDate;
+
+  // constructor
+  public Task(
+      String title,
+      String description,
+      User assignedUser,
+      Status status,
+      Taskpriority priority,
+      String dueDate) {
+    this.title = title;
+    this.description = description;
+    this.assignedUser = assignedUser;
+    this.status = status;
+    this.priority = priority;
+    this.dueDate = dueDate;
+    this.createdAt = LocalDateTime.now();
+    this.updatedAt = LocalDateTime.now();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public User getAssignedUser() {
+    return assignedUser;
+  }
+
+  public void setAssignedUser(User assignedUser) {
+    this.assignedUser = assignedUser;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
+
+  public Taskpriority getPriority() {
+    return priority;
+  }
+
+  public void setPriority(Taskpriority priority) {
+    this.priority = priority;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public String getDueDate() {
+    return dueDate;
+  }
+
+  public void setDueDate(String dueDate) {
+    this.dueDate = dueDate;
+  }
 
 }
